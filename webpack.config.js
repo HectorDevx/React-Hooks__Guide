a6ad0047ea2b7be7c75445e0e466bf29e0e67a41
Héctor Reyes <h.reyes@idventiva.com>
2020-08-09 17:56:12 -0500

    React Install

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugn = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    filename: "app.bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugn({
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
